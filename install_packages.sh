8143b81094b3683a57bdcbad9c915c985104e3fb
lucas-morin <lucas.morin222@gmail.com>
2023-10-10 11:42:32 +0200

    initial commit

--- install_packages.sh
+++ install_packages.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Pip install
-pip install pytorch==1.12.1 torchvision==0.13.1 cudatoolkit==11.3 
-pip install torch_geometric pytorch-lightning pandas jupyterlab scikit-learn matplotlib tqdm seaborn timm mahotas more_itertools
-pip install rdkit-pypi CairoSVG SmilesPE python-Levenshtein nltk ipykernel ipython rouge-score opencv-python albumentations paddleocr paddlepaddle torchsummary weighted-levenshtein pytesseract shapely
+conda install -c pytorch -y pytorch==1.12.1 torchvision==0.13.1 cudatoolkit=11.3 
+conda install -c pyg -y pyg 
+pip install torch_geometric pytorch-lightning pandas jupyterlab scikit-learn matplotlib tqdm seaborn timm mahotas more_itertools tensorboard
+pip install rdkit-pypi CairoSVG SmilesPE python-Levenshtein nltk ipykernel ipython rouge-score opencv-python albumentations paddleocr paddlepaddle torchsummary weighted-levenshtein pytesseract shapely datasets
 
 # For benchmarking, set up OSRA, Imago, MolVec, DECIMER, Img2Mol, MolScribe, CEDe
 
