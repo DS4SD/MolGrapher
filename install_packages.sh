e94fd68822c53a25eb88f97382006291e971ee1b
lucas-morin <lucas.morin222@gmail.com>
2023-11-06 11:37:12 +0100

    install with pip only

--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Pip install
-conda install -c pytorch -y pytorch==1.12.1 torchvision==0.13.1 cudatoolkit=11.3 
-conda install -c pyg -y pyg 
-pip install torch_geometric pytorch-lightning pandas jupyterlab scikit-learn matplotlib tqdm seaborn timm mahotas more_itertools tensorboard
-pip install rdkit-pypi CairoSVG SmilesPE python-Levenshtein nltk ipykernel ipython rouge-score opencv-python albumentations paddleocr paddlepaddle torchsummary weighted-levenshtein pytesseract shapely datasets
+# Install PyTorch
+python3.9 -m pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117 \
+    pytorch_lightning==2.0.2 torch-geometric==2.3.1 
+
+# Install Other Libraries
+python3.9 -m pip install scikit-learn seaborn timm mahotas more_itertools \
+    rdkit-pypi CairoSVG SmilesPE python-Levenshtein nltk ipykernel ipython \
+    rouge-score opencv-python albumentations torchsummary weighted-levenshtein \
+    pytesseract shapely datasets
+
+# Install PaddleµOCR
+python3.9 -m pip install paddleocr paddlepaddle 
 
 # For benchmarking, set up OSRA, Imago, MolVec, DECIMER, Img2Mol, MolScribe, CEDe
 
